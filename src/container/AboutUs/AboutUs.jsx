f4fad07ce0e5197fcf013a069cde211b4ac2e5fc
Teyddie22 <teyddie@gmail.com>
2022-02-11 05:38:58 +0300

    about section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
+import {images} from '../../constants'
 import './AboutUs.css';
 
 const AboutUs = () => (
-  <div>
-    AboutUs
-  </div>
+  <section className = "app__aboutus app__bg flex__center section__padding" id='about'>
+
+<div className="app__aboutus-overlay flex__center">
+<img src={images.G} alt="g"  />
+</div>
+
+<div className="app__aboutus-content flex__center">
+<div className="app__aboutus-content_about">
+<h1 className="headtext__cormorant">
+About Us
+</h1>
+<img src={images.spoon} alt="spoon_image" className="spoon__img" />
+<p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolores quae odit numquam aut eaque praesentium itaque beatae suscipit quis sunt vero consequatur, alias animi non? Sed ab impedit totam?</p>
+
+<button type="button" className='custom__button'>Know more</button>
+
+</div>
+
+<div className="app__aboutus-content_knife flex__center">
+
+<img src={images.knife} alt="knife_image" />
+</div>
+
+<div className="app__aboutus-content_history">
+<h1 className="headtext__cormorant">
+Our History
+</h1>
+<img src={images.spoon} alt="spoon_image" className="spoon__img" />
+<p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolores quae odit numquam aut eaque praesentium itaque beatae suscipit quis sunt vero consequatur, alias animi non? Sed ab impedit totam?</p>
+
+<button type="button" className='custom__button'>Know more</button>
+
+</div>
+</div>
+    
+  </section>
 );
 
 export default AboutUs;
