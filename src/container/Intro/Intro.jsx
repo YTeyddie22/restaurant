a58fbe8a12db963ee9ecf23a953bd1b501103e7f
Teddy Yongo <teyddie@gmail.com>
2023-12-16 15:58:28 +0300

    Implementing the video section

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
 
-import './Intro.css';
+import { meal } from "../../constants";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+import "./Intro.css";
+
+const Intro = () => {
+	const [playVideo, setplayVideo] = useState(false);
+	const vidRef = useRef();
+
+	function handleVideo() {
+		setplayVideo((play) => !play);
+
+		if (playVideo) {
+			vidRef.current.pause();
+		} else {
+			vidRef.current.play();
+		}
+	}
+	return (
+		<div className='app__video'>
+			<video
+				src={meal}
+				type='video/mp4'
+				ref={vidRef}
+				loop
+				controls={false}
+				muted
+			/>
+			<div className='app__video-overlay flex__center'>
+				<div
+					className='app__video-overlay_circle flex__center'
+					onClick={handleVideo}>
+					{playVideo ? (
+						<BsPauseFill color='#fff' fontSize={30} />
+					) : (
+						<BsFillPlayFill color='#fff' fontSize={30} />
+					)}
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Intro;
